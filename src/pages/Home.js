e2ae5754c3e1d9d5a9b22a6d7f9ebc8fdd5f8f70
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2022-10-07 16:42:25 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+
+import Bg from '../assets/home-bg.jpeg'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1>
+            Home
+        </h1>
+        <img src={Bg} alt="home-bg" />
+    </div>
+  )
+}
+
+export default Home
