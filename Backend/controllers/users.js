575631339d4e9ea07b4fb75eaf5375d158ec3bad
Bhanu Arora <mrbhanuarora@gmail.com>
2022-10-14 23:48:38 +0530

    one controller created

---
+++ Backend/controllers/users.js
@@ -0,0 +1,31 @@
+const users = require("../models/users");
+const axios = require("axios");
+exports.registerUser = async (req,res,next) => {
+    try {
+        let {userName} = req.body;
+        let userData = await users.findOne({
+            userName
+        });
+        if(!userData){
+            let profile = await axios.get(`https://codeforces.com/api/user.info?handles=${userName}`);
+            if(profile?.data?.result.length === 0){
+                return res.status(404).json({
+                    message:"no profile found for this handle"
+                });
+            }
+            userData = new users({
+                userName,
+                profile:profile?.data?.result[0]
+            });
+            await userData.save();
+        }
+        return res.status(200).json({
+            message:"userData fetched successfully",
+            userData
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message:error.message
+        });
+    }
+}
