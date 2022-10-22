fb09f237192d913e42eee5c5c6f1adfdcad9275f
Abhishek Vats <virus@Abhisheks-MacBook-Air.local>
2022-10-22 22:58:08 +0530

    some minon updates 2

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -109,17 +109,33 @@ export default function Home() {
         )}
         <FaArrowRight
           className={`arrow ${(roomID === "" && privateOn === false) ?
+            "disabled"
+            : `${(privateOn === true && password === "" || privateOn === true && password !== "" && roomID === "") ?
               "disabled"
-              : `${(privateOn === true && password === "" || privateOn === true && password !== "" && roomID === "") ?
-                "disabled"
-                : ""
-              }`
+              : ""
+            }`
             }`}
-          // {`navbar ${state && "navbar_black"}`}
-          onClick={handleCreateRoom}
-        >
-          Create Room
-        </FaArrowRight>
+
+          data-bs-toggle="modal" data-bs-target="#exampleModal"
+        />
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Set Room Constraits</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" onClick={handleCreateRoom}>Create Room</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
     // copy this link and send it to the person you want to connect with.Be sure to save it so you can use it later, too.
